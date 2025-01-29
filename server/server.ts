926090bfda1360962b114271bb621f4c39603665
Jeffrey Baumes <jeffbaumes@gmail.com>
2025-01-28 21:15:22 -0500

    don't do https here, use caddy for that

--- server/server.ts
+++ server/server.ts
@@ -2,22 +2,9 @@ import { WebSocketServer, WebSocket } from 'ws';
 import { decodeMessage, encodeMessage } from '../shared/messages';
 import { createLocalServerConnection, LocalServerConnection } from '../shared/serverConnection';
 import { createFileBlobStorage } from './storage';
-import * as https from 'https';
-import * as fs from 'fs';
 
 const main = async () => {
-  let config: any;
-  if (process.env.DOMAIN) {
-    const server = https.createServer({
-      cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`),
-      key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
-    });
-    config = { server };
-  } else {
-    config = { host: '0.0.0.0', port: 8080 };
-  }
-
-  const wss = new WebSocketServer(config);
+  const wss = new WebSocketServer({ port: 8080});
 
   const users = await createFileBlobStorage("./data/users");
   const worlds = await createFileBlobStorage("./data/worlds");
@@ -53,10 +40,6 @@ const main = async () => {
       localConn.send(decodeMessage(data));
     });
   });
-
-  if (config.server) {
-    config.server.listen(443);
-  }
 };
 
 main();
